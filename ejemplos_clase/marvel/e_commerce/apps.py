103a39a576483130e4e89768f4d1f051b51ef433
javierc90 <javiercarugno@gmail.com>
2022-03-16 19:30:45 -0300

    test+

--- ejemplos_clase/marvel/e_commerce/apps.py
+++ ejemplos_clase/marvel/e_commerce/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class ECommerceConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'e_commerce'
+    name = 'e_commerce' #comentario
